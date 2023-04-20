907d7fd147f4075c13d595d6e558750cf96d84fc
pratokko <pratokko1@gmail.com>
2023-04-20 17:10:06 +0300

    verifying contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const simpleStorageFactory = await ethers.getContractFactory(
@@ -10,6 +10,39 @@ async function main() {
     await simpleStorage.deployed()
 
     console.log(`deployed contract to ${simpleStorage.address}`)
+
+    if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`current value is ${currentValue}`)
+
+    // update the current value
+
+    const txResponse = await simpleStorage.store(7)
+    await txResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`updated Value is ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("already verified")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
